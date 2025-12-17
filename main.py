417a9efcfa0fb1f1438449736f8fcc8dadb77b5a
Golpezas <antonio.hernandezmm@gmail.com>
2025-12-17 14:52:12 -0300

    actualizacion de main para permitir origen desde google docs

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "https://control-flota-mac.vercel.app",
+        "https://docs.google.com",
     ], 
     allow_credentials=True,
     allow_methods=["*"],
